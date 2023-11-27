62fddb1156206571667bc1c8f8471543ebfba57e
Likoriz <129528766+Likoriz@users.noreply.github.com>
2023-11-27 18:20:37 +0300

    +box2d

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 ﻿#include <SFML/Graphics.hpp>
+#include <box2d\box2d.h>
+#include <iostream>
 
 using namespace sf;
 
 int main()
 {
+    b2Vec2 gravity(0.0f, 1.0f);
+    b2World world(gravity);
 
     RenderWindow window(VideoMode(400, 400), L"Новый проект", Style::Default);
 
